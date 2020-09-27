a03923428537a8cdaec9c008a5c159fac3979e35
ervisaaa <ervisa.cakaj@gmail.com>
2020-09-27 23:38:09 +0200

    1

---
+++ DelegatesExample/Photocs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExample
+{
+
+    public class Photo
+    {
+        public static Photo Load(string path)
+        {
+            return new Photo();
+        }
+
+        public void Save()
+        {
+
+        }
+    }
+}
---
+++ DelegatesExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExample
+{
+    class Program
+    {
+
+        //static void Main(string[] args)
+        //{
+        //    var processor = new PhotoProcessor();
+        //    processor.Process("photo.jpg");
+        //}
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            Action<Photo> filterHandler = filters.ApplyBrightness;
+            //PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+            filterHandler += filters.Resize;
+
+            processor.Process("photo.jpg", filterHandler);
+            Console.ReadLine();
+        }
+
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Apply RemoveRedEye");
+
+        }
+    }
+
+    }
